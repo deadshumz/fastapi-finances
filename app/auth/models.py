91d3debf46127edad9e12e1eb1315b07a437a613
deadshumz <shumzz@bk.ru>
2024-05-31 21:54:24 +0300

    black linter

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,8 +1,10 @@
 from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
+
 class Base(DeclarativeBase):
     pass
+
 
 class User(Base):
     __tablename__ = "user"
--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
 from pydantic import PostgresDsn, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
-    
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
     DB_USER: str | None = None
     DB_PASSWORD: str | None = None
     DB_HOST: str | None = None
@@ -11,18 +12,19 @@ class Settings(BaseSettings):
     DB_NAME: str | None = None
     pg_dsn: PostgresDsn | None = None
 
-    @field_validator('pg_dsn', mode='before')
-    def build_pg_dsn(cls, v: str | None, fields) -> PostgresDsn:
+    @field_validator("pg_dsn", mode="before")
+    def build_pg_dsn(cls, v: str | None, fields) -> str:
         if isinstance(v, str):
             return v
 
         return PostgresDsn.build(
-            scheme = 'postgresql+asyncpg',
-            username = fields.data['DB_USER'],
-            password = fields.data['DB_PASSWORD'],
-            host = fields.data['DB_HOST'],
-            port = fields.data['DB_PORT'],
-            path = fields.data['DB_NAME'],
+            scheme="postgresql+asyncpg",
+            username=fields.data["DB_USER"],
+            password=fields.data["DB_PASSWORD"],
+            host=fields.data["DB_HOST"],
+            port=fields.data["DB_PORT"],
+            path=fields.data["DB_NAME"],
         )
+
 
 settings = Settings()
