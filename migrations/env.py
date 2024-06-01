e0bb4dc8546b91db006f2270855a4ee6094f40e3
deadshumz <shumzz@bk.ru>
2024-06-01 13:26:24 +0300

    formatted env.py

--- migrations/env.py
+++ migrations/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from app.auth.models import User  # noqa: F401
 from app.config import settings
 from app.database import Base
 
